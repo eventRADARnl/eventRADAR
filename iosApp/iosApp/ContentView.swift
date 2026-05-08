033d70e797148bbcff3da6cca48d0234b6ebba63
eventRADARnl <info@bsvideography.de>
2026-05-08 22:02:48 +0200

    fix

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -7,7 +7,7 @@ struct ComposeView: UIViewControllerRepresentable {
         MainViewControllerKt.MainViewController()
     }
 
-    fun updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
