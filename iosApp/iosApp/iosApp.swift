ca070adf53d9cd90ecb27c2ccd70d8f219136425
eventRADARnl <info@bsvideography.de>
2026-05-09 01:00:31 +0200

    ios3

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -1,11 +1,28 @@
+import UIKit
 import SwiftUI
 import ComposeApp
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+                .ignoresSafeArea(.all)
+                .ignoresSafeArea(.keyboard)
+    }
 }
